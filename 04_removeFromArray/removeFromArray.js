3db238a466e5015906a4c34e2a470f1e4bfca47a
stefanpython <stefanhtml@yahoo.com>
2022-08-29 15:20:18 +0300

    Add filter function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let result = array.filter(a2 => !args.includes(a2));
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
