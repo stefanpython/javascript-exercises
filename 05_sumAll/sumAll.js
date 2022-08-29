5cad0aa997b3065594c3a8123ffda78af6b9842c
stefanpython <stefanhtml@yahoo.com>
2022-08-29 15:55:55 +0300

    Add loop to create total sum between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let total = 0;
+
+    if (a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+      }
+
+    for (let i = a; i <= b; i++) {
+        total += i
+    }
+    
+
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+
+    if (typeof a != typeof b) {
+        return "ERROR"
+    }
+
+    return total;
 };
 
 // Do not edit below this line
