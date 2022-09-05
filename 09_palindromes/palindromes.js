6703e608ca8db82974e0db609635e23a8c74309f
stefanpython <stefanhtml@yahoo.com>
2022-09-05 18:44:42 +0300

    Function to return fibonacci number

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,14 @@
 const palindromes = function (str) {
-    let re = /[\W_]/g;
+    // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+    let re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
 
+    // Step 2. Convert string to lowerCase and replace any special character with an empty space
     let lowRegStr = str.toLowerCase().replace(re, '');
 
+    // Step 3. Reverse string
     let reverseStr = lowRegStr.split('').reverse().join('');
 
+    // Step 4. Check if reversed string is equal to the initial lowerCase string
     return reverseStr === lowRegStr;
 };
 
