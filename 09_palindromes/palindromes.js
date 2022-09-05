79d0f65a58b8c231d70781aaaaf768fbed4680bf
stefanpython <stefanhtml@yahoo.com>
2022-09-05 18:14:18 +0300

    Create function to check if string is plaindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let re = /[\W_]/g;
 
+    let lowRegStr = str.toLowerCase().replace(re, '');
+
+    let reverseStr = lowRegStr.split('').reverse().join('');
+
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
