de5054ab19c74343aed53bcb9ac351f41fc58e63
stefanpython <stefanhtml@yahoo.com>
2022-09-05 17:35:57 +0300

    Create function to return factorial of n

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,12 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
