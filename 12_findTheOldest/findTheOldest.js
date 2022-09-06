6258b24d12a55fbe5dc20c4e9a373b91d078c3ae
stefanpython <stefanhtml@yahoo.com>
2022-09-06 15:52:17 +0300

    function to return the oldest person in an array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const d = new Date();
+    const currentYear = d.getFullYear();
 
-};
+    const age = (x) => (x.yearOfDeath || currentYear) - x.yearOfBirth;
 
+    const oldest = people.sort(function (a, b) {
+        if (age(a) > age(b)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return oldest[0]
+}
 // Do not edit below this line
 module.exports = findTheOldest;
